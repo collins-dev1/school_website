8b926779c46a2e1e393c6b956b358b060a1a2a21
collins-dev1 <collinsxuccessful39@gmail.com>
2025-02-12 00:46:25 +0100

    first commit

---
+++ src/navbar.js
@@ -0,0 +1,20 @@
+const Navbar = () => {
+    return ( 
+        <div className="navbar">
+            <div className="nav-logo">
+                <img></img>
+            </div>
+            <div className="nav-link">
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="">About Us</a></li>
+                    <li><a href="">Contact Us</a></li>
+                    <li><a href="">Blog</a></li>
+                    <li><a href="">Register</a></li>
+                </ul>
+            </div>
+        </div>
+     );
+}
+ 
+export default Navbar;
