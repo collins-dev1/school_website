1abe3888d461c7715276e8df2933d038533cbf4c
collins-dev1 <collinsxuccessful39@gmail.com>
2025-02-23 00:05:03 +0100

    card

--- src/home.js
+++ src/home.js
@@ -3,6 +3,12 @@ import school_pic1 from "./asset/school_picture2.jpeg";
 import school_pic2 from "./asset/school_picture3.jpeg";
 import line from "./asset/line.png";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import course1 from "./asset/course1.jpeg";
+import course2 from "./asset/course2.jpeg";
+import course3 from "./asset/course3.jpeg";
+import course4 from "./asset/course4.jpeg";
+import course5 from "./asset/course5.jpeg";
+import course6 from "./asset/course6.jpeg";
 
 
 const Home = () => {
@@ -106,12 +112,99 @@ const Home = () => {
             </div>
 
             {/* Popular Courses */}
-            <div>
+            <div className="container">
                 <div className="popular_course">
                     <h3>Our Popular Courses</h3>
                     <Link>
                         <button>VIEW ALL</button>
                     </Link>
+                </div>
+
+                <div className="row row-cols-1 row-cols-md-4 row-cols-lg-3 g-3 mt-3">
+                    <div className="col" id="card-holder">
+                        <div className="card">
+                            <div>
+                                <img src={course1} width="100%"/>
+                            </div>
+                            <div className="card-body">
+                                <span>UI/UX Course</span>
+                                <h5>$450</h5>
+                            </div>
+                            <div className="card-footer">
+                                <h4>UI/UX Complete : From Zeo to Hero</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col" id="card-holder">
+                        <div className="card">
+                            <div>
+                                <img src={course2} width="100%"/>
+                            </div>
+                            <div className="card-body">
+                                <span>UI/UX Course</span>
+                                <h5>$450</h5>
+                            </div>
+                            <div className="card-footer">
+                                <h4>UI/UX Complete : From Zeo to Hero</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col" id="card-holder">
+                        <div className="card">
+                            <div>
+                                <img src={course3} width="100%"/>
+                            </div>
+                            <div className="card-body">
+                                <span>UI/UX Course</span>
+                                <h5>$450</h5>
+                            </div>
+                            <div className="card-footer">
+                                <h4>UI/UX Complete : From Zeo to Hero</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col" id="card-holder">
+                        <div className="card">
+                            <div>
+                                <img src={course4} width="100%"/>
+                            </div>
+                            <div className="card-body">
+                                <span>UI/UX Course</span>
+                                <h5>$450</h5>
+                            </div>
+                            <div className="card-footer">
+                                <h4>UI/UX Complete : From Zeo to Hero</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col" id="card-holder">
+                        <div className="card">
+                            <div>
+                                <img src={course5} width="100%"/>
+                            </div>
+                            <div className="card-body">
+                                <span>UI/UX Course</span>
+                                <h5>$450</h5>
+                            </div>
+                            <div className="card-footer">
+                                <h4>UI/UX Complete : From Zeo to Hero</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col" id="card-holder">
+                        <div className="card">
+                            <div>
+                                <img src={course6} width="100%"/>
+                            </div>
+                            <div className="card-body">
+                                <span>UI/UX Course</span>
+                                <h5>$450</h5>
+                            </div>
+                            <div className="card-footer">
+                                <h4>UI/UX Complete : From Zeo to Hero</h4>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </main>
--- src/navbar.js
+++ src/navbar.js
@@ -1,25 +1,25 @@
 import my_image from "./asset/ogcollinscollegemain.png";
-import {Link} from "react-router-dom";
-import React, {useState, useEffect} from "react";
+import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 
 const Navbar = () => {
     const [navbar, setNavbar] = useState(false);
     const [navlink, setNavlink] = useState(false);
 
     const changeNavlink = () => {
-        if(window.scrollY >= 80){
+        if (window.scrollY >= 80) {
             setNavlink(true);
         }
-        else{
+        else {
             setNavlink(false);
         }
     };
 
     const changeBackground = () => {
-        if(window.scrollY >= 80){
+        if (window.scrollY >= 80) {
             setNavbar(true);
         }
-        else{
+        else {
             setNavbar(false)
         }
     };
@@ -38,7 +38,7 @@ const Navbar = () => {
         }
     }, []);
 
-    return ( 
+    return (
         <nav className={navbar ? "navba navbar-active" : "navba"}>
             <div className="nav-logo">
                 <Link to="/">
@@ -62,7 +62,37 @@ const Navbar = () => {
                 </a>
             </div>
         </nav>
-     );
+        // <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        //     <div className={navbar ? "navba navbar-active" : "navba"}>
+        //             <Link to="/" className="navbar-brand">
+        //                 <img src={my_image} width="100px" height="100px" />
+        //             </Link>
+        //             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        //                 <span class="navbar-toggler-icon"></span>
+        //             </button>
+        //             <div className={navlink ? "nav-link nav-link-active" : "nav-link"}>
+        //                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        //                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        //                         <li><Link to="/">Home</Link></li>
+        //                         <li><Link to="/about">About Us</Link></li>
+        //                         <li><Link to="">Contact Us</Link></li>
+        //                         <li><Link to="">Blog</Link></li>
+        //                     </ul>
+        //                     <div className="nav-button">
+        //                         <a href="">
+        //                             <button>Login</button>
+        //                         </a>
+        //                         <a href="">
+        //                             <button>Register</button>
+        //                         </a>
+        //                     </div>
+        //                 </div>
+        //             </div>
+
+        //         </div>
+        // </nav>
+
+    );
 }
- 
+
 export default Navbar;
