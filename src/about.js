00db2b756cdb5e9991a00bd0233584ce523d2a8c
collins-dev1 <collinsxuccessful39@gmail.com>
2025-02-24 00:23:03 +0100

    about us 2

--- src/about.js
+++ src/about.js
@@ -1,9 +1,11 @@
 const AboutUs = () => {
     return (
-        <div>
+        <div className="about-container">
             <div className="about-pic">
+                <div className="about-text">
                 <h1 className="display-5">About Us</h1>
                 <p>Our school is dedicated to providing a nurturing learning environment that fosters academic success, creativity, and character development.</p>
+                </div>
             </div>
         </div>
     );
