5b140435efe9bd65981488060252b8c42fa00326
collins-dev1 <collinsxuccessful39@gmail.com>
2025-02-24 23:53:12 +0100

    about second

--- src/about.js
+++ src/about.js
@@ -1,3 +1,5 @@
+import graduated from "./asset/graduated_student.jpeg";
+
 const AboutUs = () => {
     return (
         <div className="about-container">
@@ -5,6 +7,14 @@ const AboutUs = () => {
                 <div className="about-text">
                 <h1 className="display-5">About Us</h1>
                 <p>Our school is dedicated to providing a nurturing learning environment that fosters academic success, creativity, and character development.</p>
+                </div>
+            </div>
+            <div className="about-second">
+                <div>
+
+                </div>
+                <div>
+                    <img src={graduated}/>
                 </div>
             </div>
         </div>
