2ff9b8fe568c4b1ee933e81363468534e5030227
collins-dev1 <collinsxuccessful39@gmail.com>
2025-02-26 22:42:53 +0100

    contact 3

--- src/contact.js
+++ src/contact.js
@@ -1,4 +1,4 @@
-
+import school_building from "./asset/school_building.jpeg";
 
 const Contact = () => {
     return (
@@ -11,7 +11,20 @@ const Contact = () => {
             </div>
             <div>
                 <div>
-                    
+                    <img src={school_building} />
+                </div>
+                <div>
+                    <div>
+                        <h1>Contact Us</h1>
+                        <p>Your "Contact Us" header text should be clear, inviting, and professional. It sets the tone for how visitors reach out to you. Here’s a short description:
+
+                            "Have questions or need assistance? Get in touch with us! We're here to help and look forward to hearing from you."
+
+                            Let me know if you want something more formal, friendly, or tailored to your business </p>
+                    </div>
+                    <div>
+                        
+                    </div>
                 </div>
             </div>
         </div>
