e06d1a822ffbc92b987050f317924acd652c2302
collins-dev1 <collinsxuccessful39@gmail.com>
2025-02-28 00:12:14 +0100

    real contact us

--- src/contact.js
+++ src/contact.js
@@ -9,11 +9,11 @@ const Contact = () => {
                     <p>Have questions or need assistance? Get in touch with us! We're here to help and look forward to hearing from you.</p>
                 </div>
             </div>
-            <div>
+            <div className="main-contact">
                 <div>
                     <img src={school_building} />
                 </div>
-                <div>
+                <div className="contact-texting">
                     <div>
                         <h1>Contact Us</h1>
                         <p>Your "Contact Us" header text should be clear, inviting, and professional. It sets the tone for how visitors reach out to you. Here’s a short description:
@@ -22,8 +22,26 @@ const Contact = () => {
 
                             Let me know if you want something more formal, friendly, or tailored to your business </p>
                     </div>
-                    <div>
-                        
+                    <div className="mt-4">
+                        <form>
+                            <div className="full-name">
+                                <div>
+                                    <label>Full Name</label>
+                                    <input type="text" placeholder="Full Name" className="form-control"/>
+                                </div>
+                                <div>
+                                    <label>Email Address</label>
+                                    <input type="email" placeholder="Email Address" className="form-control"/>
+                                </div>
+                            </div>
+                            <div className="mt-4">
+                                <label>Text Message</label>
+                                <textarea className="form-control"></textarea>
+                            </div>
+                            <div className="form-button mt-4">
+                                <button>Get in Touch</button>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
