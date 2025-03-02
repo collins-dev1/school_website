499873073484dbe8b13a253397306c11d65ff34f
collins-dev1 <collinsxuccessful39@gmail.com>
2025-03-03 00:01:17 +0100

    signup

---
+++ src/signup.js
@@ -0,0 +1,27 @@
+const Signup = () => {
+    return ( 
+        <div className="signup-main">
+            <div className="signup-text">
+                <h1>Hello!</h1>
+                <p>Welcome to OG COLLINS College</p>
+            </div>
+            <div className="signup-container">
+                <h3>Login</h3>
+                <div>
+                    <input type="email" className="form-control" placeholder="Email" />
+                </div>
+                <div>
+                    <input type="password" className="form-control" placeholder="Password" />
+                </div>
+                <div>
+                    <Link>Forget Password</Link>
+                </div>
+                <div>
+                    <button>Register</button>
+                </div>
+            </div>
+        </div>
+     );
+}
+ 
+export default Signup;
